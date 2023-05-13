115dcfea827d0abc3328241725e940c349b75ae8
Xero kun <elo48858@gmail.com>
2023-05-13 14:55:47 +0100

    `🍜FIX` Code Refactoring

--- src/core/StartupGame.java
+++ src/core/StartupGame.java
@@ -10,13 +10,14 @@ public class StartupGame {
     int numOfGuesses = 0;
 
     public void setupGame() {
-        System.out.println("Sink-A-Startup");
+        System.out.println("Sink-A-Startup\n");
         System.out.println("How many of these start-ups can you sink with the least number of guesses?");
+        System.out.println("\n - THE RULES -");
         System.out.println("\tThe rules are simple, there are three start-ups, each occupying three spaces in a 7x7 grid.");
         System.out.println("\tYour goal is to sink these start-ups with the least number of guesses.");
         System.out.println("\tYou'll have to guess the positions of the start-ups, for example, C3, C4, C5, in order to sink it");
         System.out.println("\tThe game ends when you've sunk all three startups.");
-        System.out.println("\tGood luck!");
+        System.out.println("\tGood luck!\n");
 
         Startup hakki = new Startup("hakki");
         Startup tableSaltAI = new Startup("table-salt AI"); // Reigen Arataka might actually own this one lol
@@ -34,7 +35,7 @@ public class StartupGame {
     public void startPlaying() {
         setupGame();
         while (!startups.isEmpty()) {
-            String userGuess = helper.getUserInput("Your guess");
+            String userGuess = helper.getUserInput("Your Guess");
             checkUserGuess(userGuess);
         }
         finishGame();
@@ -45,18 +46,24 @@ public class StartupGame {
             String result = startup.checkYourself(userGuess);
             numOfGuesses++;
             if (result.equals("hit")) {
-                System.out.println("Cell " + userGuess + " : hit!");
+                System.out.println("Cell " + userGuess.toUpperCase() + ", hit!");
                 break;
             } else if (result.equals("kill")) {
                 startups.remove(startup);
-                System.out.println("Ouch, you sunk " + startup.name + " !");
+                System.out.println("Ouch, you sunk " + startup.name + "!");
                 break;
+            } else {
+                System.out.println("Miss! Haha, nice shot!");
             }
         }
     }
 
     public void finishGame() {
         System.out.println("You've sank all the start-ups, your stocks are now worthless!");
-        System.out.println("Heh. Took you long enough, " + numOfGuesses + " guesses.");
+        if (numOfGuesses <= 20) {
+            System.out.println("Only took you " + numOfGuesses + " guesses?");
+        } else {
+            System.out.println("Heh. Took you long enough, " + numOfGuesses + " guesses.");
+        }
     }
 }
